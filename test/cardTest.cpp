8f67d41aede5dd100affb661583d979c34414137
anj501 <anjustin28@gmail.com>
2023-03-01 15:39:36 -0800

    card.cpp hotfix

---
+++ test/cardTest.cpp
@@ -0,0 +1,29 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "../header/card.hpp"
+
+TEST(cardTest1, testConstructor1){
+    EXPECT_NO_THROW(card *cardPTR = new card());
+}
+
+TEST(cardTest2, testConstructor2){
+    EXPECT_NO_THROW(card *cardPTR = new card(3214, "Ace", "Spade"));
+}
+
+TEST(cardTest3, test_setValue_and_getValue){
+    card *cardPTR = new card();
+    cardPTR->setValue(123);
+    EXPECT_EQ(cardPTR->getValue(), 123);
+}
+
+TEST(cardTest4, test_setName_and_getName){
+    card *cardPTR = new card();
+    cardPTR->setName("Ace");
+    EXPECT_EQ(cardPTR->getName(), "Ace");
+}
+
+TEST(cardTest5, test_setSuit_and_getSuit){
+    card *cardPTR = new card();
+    cardPTR->setSuit("Spades");
+    EXPECT_EQ(cardPTR->getSuit(), "Spades");
+}
