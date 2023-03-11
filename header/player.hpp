40e9865e01b829fb28652e0c13f10b76f437e423
awu133 <awu133@ucr.edu>
2023-03-10 20:29:33 -0800

    Modified player.hpp and player.cpp
    Declared function names in player.hpp and player.cpp

---
+++ header/player.hpp
@@ -0,0 +1,34 @@
+#ifndef PLAYER_HPP
+#define PLAYER_HPP
+
+#include "card.hpp"
+#include "deck.hpp"
+#include "hand.hpp"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+class player{
+    public:
+        string name;
+        int money;
+        vector<hand*> handV;
+
+        player();
+        ~player();
+
+        string getName();
+        int viewMoney();
+        int numHands();
+        void wonBet();
+
+        void hit();
+        void stand();
+        void doubleDown();
+        void split();
+        void surrender();
+
+}
+
+#endif
